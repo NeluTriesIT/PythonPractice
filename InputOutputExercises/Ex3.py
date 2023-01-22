a58aa84091f5a0df150c90eff755e4d9be3671da
Nelu <nelutriesit@gmail.com>
2023-01-22 23:04:47 +0200

    Input/Output Exercise 3

---
+++ InputOutputExercises/Ex3.py
@@ -0,0 +1,4 @@
+#Convert Decimal number to octal using print() output formatting
+n = int(input("Decimal number to be converted in octal: "))
+
+print(f"The octal converted decimal number is: " '%o'%n)   # '%o'%n -->Outputs the number in base 8.

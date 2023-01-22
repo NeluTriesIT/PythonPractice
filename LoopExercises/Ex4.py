6b3f097b13203b67b8df5a76028de761f79fba9b
Nelu <nelutriesit@gmail.com>
2023-01-23 01:15:56 +0200

    Loop Exercise 4

---
+++ LoopExercises/Ex4.py
@@ -0,0 +1,5 @@
+#Print multiplication table of a given number
+n = int(input("insert number = "))
+e = int(input("insert the number of multiplications = "))
+for i in range(1, e+1):
+    print(n * i)

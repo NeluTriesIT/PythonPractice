6f57cb02c33da96d9d25266a1aaff2cfd31e79da
Nelu <nelutriesit@gmail.com>
2023-01-22 22:07:35 +0200

    Basic Exercise 15

---
+++ BasicEx15.py
@@ -0,0 +1,6 @@
+#Write a function called exponent(base, exp) that returns an int value of base raises to the power of exp.
+#base = integer and exp = non negative integer
+base = int(input("Base = "))
+exp = int(input("Exponent = "))
+
+print(f"{base} raised to the power of {exp} is : {base ** exp}")

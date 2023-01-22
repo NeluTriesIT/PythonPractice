c5566ba364fdc72ed41d4a6c7a52409cc77760ef
Nelu <nelutriesit@gmail.com>
2023-01-22 22:40:06 +0200

    Moved Ex1 to branch directory

--- Ex1.py
+++
@@ -1,4 +0,0 @@
-#Write a program to accept two numbers from the user and calculate multiplication
-a = int(input("First number: "))
-b = int(input("Second number: "))
-print(f"Multiplication is : {a * b}")

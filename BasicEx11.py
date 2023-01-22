abf8ff8bfb91b1680f63ccaa95dc7be6be92b241
Nelu <nelutriesit@gmail.com>
2023-01-22 17:49:41 +0200

    Basic Exercise 11

---
+++ BasicEx11.py
@@ -0,0 +1,4 @@
+#Write a program to extract each digit from an integer in the reverse order
+#7536, output will be 6 3 5 7, with a space separating the digits
+
+print(" ".join(reversed(input("Please provide a number to be reversed: "))))
